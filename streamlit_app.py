7fbcfb716c87bcd825c96d6ede419af66be0b2ee
Warren B Rebello <68852540+warrenrebello@users.noreply.github.com>
2023-10-18 21:00:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon' + fruit_choice)
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
